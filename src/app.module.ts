a08216ff6be12a28aa355e3b0fc33f90f162272b
Douglas Batista <douglas.silva@zooxsmart.com>
2024-10-17 21:11:14 -0300

    feat: PostgresSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './infra/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
