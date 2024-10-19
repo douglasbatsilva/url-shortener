bebf933f27c12d50e85c5b2553e0048dbb26547e
Douglas Batista <douglas.silva@zooxsmart.com>
2024-10-19 09:27:54 -0300

    feat: fastify no main.ts, core da aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { abortOnError: false, bufferLogs: true },
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,6 +18,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
