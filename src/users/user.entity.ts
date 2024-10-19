6d8db034b12ddc5267613dc84eb94bfc8b825548
Douglas Batista <douglas.silva@zooxsmart.com>
2024-10-19 09:30:29 -0300

    feat: correlação entre a tabela de user com a de url

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Url } from '../url/url.entity';
 
 @Entity()
 export class User {
@@ -13,6 +14,15 @@ export class User {
 
   @Column()
   password: string;
+
+  @Column({ default: false })
+  deleted: boolean;
+
+  @Column({ type: 'timestamp', nullable: true })
+  deletedAt: Date | null;
+
+  @OneToMany(() => Url, (url) => url.author)
+  urls: Url[];
 
   @CreateDateColumn({ type: 'timestamp' })
   createdAt: Date;
