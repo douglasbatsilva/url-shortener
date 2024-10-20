1a64e5cf3f8a81a60defd40c9d1dbe8268f8082d
Douglas Batista <douglas.silva@zooxsmart.com>
2024-10-20 13:37:06 -0300

    doc: user

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,18 +1,33 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UserRegisterDTO {
-  @IsString()
-  @IsNotEmpty()
-  @Transform(({ value }) => value?.toLocaleLowerCase())
-  name: string;
-
+export class UserLoginDTO {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'johndoe@example.com',
+    description: 'The email of the user',
+  })
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'password123',
+    description: 'The password of the user',
+  })
   @MinLength(8, { message: 'Password must be at least 8 characters' })
   password: string;
+}
+
+export class UserRegisterDTO extends UserLoginDTO {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'johndoe',
+    description: 'The username',
+  })
+  @Transform(({ value }) => value?.toLocaleLowerCase())
+  name: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,33 @@
 import { Controller, Post, Body, Res } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UserRegisterDTO } from './dto/user.dto';
+import { UserLoginDTO, UserRegisterDTO } from './dto/user.dto';
 import { FastifyReply } from 'fastify';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('user')
 export class UsersController {
   constructor(private readonly service: UsersService) {}
 
   @Post('signup')
   @Post()
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({ status: 201, description: 'User successfully created.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiResponse({ status: 412, description: 'User already exists.' })
+  @ApiBody({ type: UserRegisterDTO })
   async create(@Body() body: UserRegisterDTO, @Res() reply: FastifyReply) {
     const resp = await this.service.create(body);
     reply.status(201).send(resp);
   }
 
   @Post('login')
+  @ApiOperation({ summary: 'Login a user' })
+  @ApiResponse({ status: 200, description: 'User token' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  @ApiBody({ type: UserLoginDTO })
   async login(
-    @Body() body: Partial<UserRegisterDTO>,
+    @Body() body: UserLoginDTO,
     @Res() reply: FastifyReply,
   ) {
     const resp = await this.service.login(body);
